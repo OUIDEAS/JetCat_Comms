45256eb36cf674ebb31a804c374bb6263472a175
ColtonWright51 <cw717317@ohio.edu>
2023-03-15 15:57:10 -0400

    Calibrating rc_benchmark

---
+++ rc_benchmark_curve.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import sys
+import os
+import scipy
+from scipy.signal import argrelextrema
+import matplotlib.pyplot as plt
+
+print("Input:", sys.argv[1])
+
+folder_path = str(sys.argv[1]) # Full path to file
+
+f1 = pd.read_csv(folder_path, delimiter=',', on_bad_lines='skip')
+
+f1 = f1.interpolate(method='linear')
+
+
+f1["dThrust"] = f1["Thrust (kgf)"].diff()
+f1["dThrust peaks"] = f1["dThrust"][f1["dThrust"] > 0.005]
+f1["dThrust valleys"] = f1["dThrust"][f1["dThrust"] < -0.005]
+f1.fillna(0, inplace=True)
+f1["dThrust spikes"] = f1["dThrust peaks"] + f1["dThrust valleys"]
+f1.fillna(0, inplace=True)
+c_max_index = argrelextrema(f1["dThrust spikes"], np.greater, order=5)
+
+
+f1.plot(x="Time (s)", y="Thrust (kgf)")
+f1.plot(x="Time (s)", y="dThrust")
+f1.plot(x ="Time (s)", y="dThrust peaks")
+f1.plot(x ="Time (s)", y="dThrust valleys")
+f1.plot(x ="Time (s)", y="dThrust spikes")
+
+
+
+plt.show()
